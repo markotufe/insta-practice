6b4395fdcdfa35a84a10c91679bbcab326a5c511
markic23 <tufe23@gmail.com>
2021-10-14 16:14:34 +0200

    dodat tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import RoutesContainer from "./routes/";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { setUser } from "./redux/slices/userSlice";
 import { useDispatch } from "react-redux";
 import { db } from "./firebase";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const auth = getAuth();
   const dispatch = useDispatch();
 
@@ -22,15 +24,23 @@ function App() {
           id: doc.id,
         }));
         dispatch(setUser(results[0]));
+        setLoading(false);
       } else {
         dispatch(setUser({}));
+        setLoading(false);
       }
     });
-
     return () => unsub();
   }, [auth, dispatch]);
 
-  return <RoutesContainer />;
+  return loading ? (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <Loader />
+      <p className="mt-5 text-xl font-bold text-gray-900">Preparing data...</p>
+    </div>
+  ) : (
+    <RoutesContainer />
+  );
 }
 
 export default App;
---
+++ src/components/Loader.js
@@ -0,0 +1,7 @@
+import Loader from "react-loader-spinner";
+
+const LoaderComponent = () => {
+  return <Loader type="Bars" color="#1C2B2D" height={100} width={100} />;
+};
+
+export default LoaderComponent;
