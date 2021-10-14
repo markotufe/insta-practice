d20d6f83744519b87c30d300fa9759ae70bad924
markic23 <tufe23@gmail.com>
2021-10-14 16:17:53 +0200

    dodat login ekran

--- src/screens/unauthorized/Login.js
+++ src/screens/unauthorized/Login.js
@@ -1,5 +1,23 @@
-const Login = () => {
-  return <div>Login Screen</div>;
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { setUserError } from "../../redux/slices/userSlice";
+
+const LoginScreen = () => {
+  const auth = getAuth();
+  const dispatch = useDispatch();
+
+  //registration
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, "marko001@live.com", "12345678");
+    } catch (error) {
+      dispatch(setUserError(error?.message));
+    }
+  };
+
+  return <button onClick={handleLogin}>Login</button>;
 };
 
-export default Login;
+export default LoginScreen;
