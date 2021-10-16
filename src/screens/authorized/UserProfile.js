c98188b0e746851965a5fb7242eb51f0c8d5e0fd
markic23 <tufe23@gmail.com>
2021-10-16 15:08:56 +0200

    profile

--- src/screens/authorized/UserProfile.js
+++ src/screens/authorized/UserProfile.js
@@ -38,8 +38,8 @@ const UserProfile = () => {
   };
 
   return (
-    <div>
-      {followingUsers.map((user) => {
+    <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-4 xl:max-w-6xl mx-auto pt-6 min-h-screen">
+      {/* {followingUsers.map((user) => {
         return (
           <FollowingUsers
             key={user?.userId}
@@ -47,7 +47,11 @@ const UserProfile = () => {
             handleUnfollow={handleUnfollow}
           />
         );
-      })}
+      })} */}
+      <div className="col-span-1 bg-red-700">
+        <div className="fixed">levo</div>
+      </div>
+      <div className="col-span-3 bg-yellow-400">desno</div>
     </div>
   );
 };
