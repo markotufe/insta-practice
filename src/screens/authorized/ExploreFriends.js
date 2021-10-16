987edca96dc3b13b651ac5eac0932ad6e77cb2cd
markic23 <tufe23@gmail.com>
2021-10-16 13:42:24 +0200

    Loader

--- src/screens/authorized/ExploreFriends.js
+++ src/screens/authorized/ExploreFriends.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UsersToFollow from "../../components/UsersToFollow";
+import Loader from "../../components/Loader";
 import { getUsersToFollow } from "../../helpers/getUsersToFollow";
 import { followUser } from "../../helpers/followUnfollowUser";
 import { updateUserFollowing } from "../../redux/slices/userSlice";
@@ -13,12 +14,14 @@ const ExploreFriends = () => {
     documentId: activeUserDocumentId,
   } = useSelector((state) => state.user.userData);
   const [usersToFollow, setUsersToFollow] = useState();
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
     async function suggestedProfiles() {
       const response = await getUsersToFollow(following, activeUserId);
       setUsersToFollow(response);
+      setLoading(false);
     }
 
     suggestedProfiles();
@@ -37,17 +40,30 @@ const ExploreFriends = () => {
 
   return (
     <div className="mb-5">
-      <h1 className="text-center my-10 uppercase text-3xl">Explore users</h1>
+      {!loading ? (
+        <>
+          <h1 className="text-center my-10 uppercase text-3xl">
+            Explore users
+          </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-4/5 mx-auto">
-        {usersToFollow?.map((user) => (
-          <UsersToFollow
-            key={user?.userId}
-            user={user}
-            handleFollow={handleFollow}
-          />
-        ))}
-      </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-4/5 mx-auto">
+            {usersToFollow?.map((user) => (
+              <UsersToFollow
+                key={user?.userId}
+                user={user}
+                handleFollow={handleFollow}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          <Loader />
+          <p className="mt-5 text-xl font-bold text-gray-900">
+            Fetching users...
+          </p>
+        </div>
+      )}
     </div>
   );
 };
