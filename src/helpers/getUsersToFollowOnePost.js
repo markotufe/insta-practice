19354a7e8977b89bc95f1557efdd588b539167f2
markic23 <tufe23@gmail.com>
2021-10-22 11:00:50 +0200

    izmene

--- src/helpers/getUsersToFollowOnePost.js
+++ src/helpers/getUsersToFollowOnePost.js
@@ -1,10 +1,14 @@
-import { collection, getDocs, where, query } from "firebase/firestore";
+import { collection, getDocs, where, query, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
 
 export async function getUsersToFollowOnePost(userId) {
   try {
     const collectionRef = collection(db, "posts");
-    const q = query(collectionRef, where("creatorId", "==", userId));
+    const q = query(
+      collectionRef,
+      where("creatorId", "==", userId),
+      orderBy("timestamp", "desc")
+    );
 
     const snapshot = await getDocs(q);
 
